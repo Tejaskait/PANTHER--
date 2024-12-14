dff439f22da9352d9c9992488a63c38bd6bfb71a
Tejas <tejaskait@gmail.com>
2024-12-14 18:09:56 +0530

    done frontend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
 export default function App() {
-  return (
-    <div className='text-red-800 font-nmregular'>Ap</div>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    < Route path="/sign-up" element={<SignUp />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/profile" element={<Profile />} />
+  </Routes>
+
+  </BrowserRouter>;
+  
 }
